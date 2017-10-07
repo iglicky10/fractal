60c60306adeaaff7ee8212e47d3a698aeec601e0
iglicky10 <iglicky10@gmail.com>
2017-10-07 18:20:18 -0300

    Nova versao

--- ep1_pronto.py
+++ ep1_pronto.py
@@ -2,40 +2,69 @@
 
 import turtle
 import random
+manual='''
+Bem vindo ao desenhador de fractais!!
+Este programa funciona com uma entrada, regra e repetiçoes que retornam um desenho.
+Manual:
+    Comandos de direçao:
+        f = Ir para frente
+        - = Virar 90 graus para direita
+        + = Virar 90 graus para esquerda
+        * = Virar 60 graus para direita
+        / = Virar 60 graus para esquerda
+        . = Virar 45 graus para direita
+        , = Virar 45 graus para esquerda
+    Cores:
+         r = Vermelho
+         g = Verde
+         b = Azul
+         o = Laranja
+         p = Roxo
+         y = Amarelo
+         k = Preto
+         x = Cor aleatória
+    
+   
+ENTRADA: F-F-F-F
+REGRA: F F-F+F+FF-F-F+F
+PASSOS: 2
 
-print('ENTRADA: F-F-F-F')
-print('REGRA: F yF-pF+xF+bFoF-rF-gF+bF ')
-turtle.setpos(-400,0)
+Teste agora com cores!
+
+ENTRADA: F-F-F-F
+REGRA: F xF-xF+xF+xFxF-xF-xF+xF
+PASSOS: 2
+
+Teste mudando o numero de passos!!
+
+Obs: Pode escrever em letras minúsculas!!
+Obs2: Fique a vontade em criar a sua entrada e regra!!
+'''
+print(manual)
+turtle.shape('turtle')
 pen = turtle.Pen()
 def get_color():
     colors  = ["red","green","blue","orange","purple","pink","yellow","black"]
     return random.choice(colors)
 
 inicial = str(input("Entrada: "))
-regra1 =str(input("regra1: "))
-regra2 =str(input("regra2: "))
+regra =str(input("regra: "))
 passos=int(input("passos: "))
 
 final=""
-regra1 = regra1.split(' ')
-regra2 = regra2.split(' ')
+regra = regra.split(' ')
+
 
 i=0
 while i < passos:
-    final = inicial.replace(regra1[0],regra1[1])
-    final = inicial.replace(regra2[0],regra2[1])
+    final = inicial.replace(regra[0],regra[1])
     inicial=final
     i+=1
 
 print(final)
 turtle.speed('fast')
-for i in final:
-    if i=='f':
-        turtle.penup()
-        turtle.forward(10)
 
-#for i in final.upper(): 
-for i in final:            
+for i in final.upper():           
     if i =='F':
        turtle.pendown() 
        turtle.forward(10)
@@ -68,4 +97,4 @@ for i in final:
     elif i=='X':
         turtle.color(get_color())
         
-        
+       
